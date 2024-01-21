c8be0b28ca1f9235ed1f136062c6b76dd55503df
Blizz <ellamsbliss@gmail.com>
2024-01-21 11:55:03 +0100

    setting up router

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -20,7 +21,9 @@ const Landing = () => {
             consequuntur cupiditate repudiandae facilis nisi similique amet
             distinctio, obcaecati nostrum animi officia.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to={"/register"} className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
